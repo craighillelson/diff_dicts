d5c24a059c61124dfdf11d2cf76bdddb2facd2ba
craighillelson <craighillelson@gmail.com>
2020-04-28 21:25:43 -0400

    update diff_dicts.py
    - edited doc string
    - edited doct string for write_to_csv function
    - changed k, v in to player, jersey_number in write_to_csv function

--- diff_dicts.py
+++ diff_dicts.py
@@ -1,22 +1,17 @@
-""" __doc__ """
+"""Finds the differences between two dictionaries and writes them to a csv."""
 
 import csv
 
 
 def write_to_csv(name_of_file, dct):
-    """ write dictionary to csv """
+    """Write dictionary to csv."""
     with open(name_of_file, 'w') as out_file:
         out_csv = csv.writer(out_file)
-        out_csv.writerow(HEADERS) # define HEADERS before running function
-        for k, v_number in dct.items():
-            keys_values = (k, v_number)
+        out_csv.writerow(['player', 'number'])
+        for player, jersey_num in dct.items():
+            keys_values = (player, jersey_num)
             out_csv.writerow(keys_values)
 
-
-HEADERS = [
-    'player',
-    'number',
-    ]
 
 DCT1 = {
     'boomer': '7',
@@ -34,3 +29,4 @@ DIFF2 = {k: DCT2[k] for k in set(DCT2) - set(DCT1)}
 MERGED_DIFFS = {**DIFF1, **DIFF2}
 
 write_to_csv('diffs.csv', MERGED_DIFFS)
+print('"diffs.csv" exported successfully')
