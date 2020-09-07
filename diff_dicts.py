c45b4d4b6465b6d6cf1c8bb18f09d630e5561aed
craighillelson <craighillelson@gmail.com>
2020-09-07 10:01:07 -0400

    update diff_dicts.py
    simplified code

--- diff_dicts.py
+++ diff_dicts.py
@@ -3,30 +3,36 @@
 import csv
 
 
-def write_to_csv(file_name, dct):
+def diff_dictionaries(DCT1, DCT2):
+    """Output a dictionary of the differences between two dictionaries."""
+    return {player: DCT1[player] for player in set(DCT1) - set(DCT2)}
+
+
+def write_to_csv(dct):
     """Write dictionary to csv."""
-    with open(file_name, 'w') as out_file:
+    with open("diffs.csv", "w") as out_file:
         out_csv = csv.writer(out_file)
-        out_csv.writerow(['player', 'number'])
+        out_csv.writerow(["player", "number"])
         for player, jersey_number in dct.items():
             keys_values = (player, jersey_number)
             out_csv.writerow(keys_values)
 
+    print('\n"diffs.csv" exported successfully\n')
+
 
 DCT1 = {
-    'boomer': '7',
-    'muñoz': '78',
+    "boomer": "7",
+    "muñoz": "78",
     }
 
 DCT2 = {
-    'montana': '16',
-    'boomer': '7',
+    "montana": "16",
+    "boomer": "7",
     }
 
-DIFF1 = {k: DCT1[k] for k in set(DCT1) - set(DCT2)}
-DIFF2 = {k: DCT2[k] for k in set(DCT2) - set(DCT1)}
+DIFF1 = diff_dictionaries(DCT1, DCT2)
+DIFF2 = diff_dictionaries(DCT2, DCT1)
 
 MERGED_DIFFS = {**DIFF1, **DIFF2}
 
-write_to_csv('diffs.csv', MERGED_DIFFS)
-print('"diffs.csv" exported successfully')
+write_to_csv(MERGED_DIFFS)
